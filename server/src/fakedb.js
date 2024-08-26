47f6413678e600a2b73615ce557b0a86498e450e
Swag-blips <coderblip@gmail.com>
2024-08-26 12:01:09 +0100

    signup route complete

---
+++ server/src/fakedb.js
@@ -0,0 +1,9 @@
+const fakedb = [
+  {
+    id: 1,
+    email: "bakrinolasupoayomide@gmail.com",
+    password: "$2a$10$6CdFyoOUOuov6CZu1AhkI.Yc5mlv3r4Ydh/mPEKB2qiMA4QiZUyeO",
+  },
+];
+
+export default fakedb;
--- server/src/server.js
+++ server/src/server.js
@@ -4,11 +4,10 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import jsonwebtoken from "jsonwebtoken";
 import bcryptjs from "bcryptjs";
+import fakedb from "./fakedb.js";
 
 const { compare, hash } = bcryptjs;
 const { verify } = jsonwebtoken;
-
-// 1. register a user
 
 // 2. login a user
 
@@ -32,9 +31,51 @@ server.use(
 );
 
 // Needed to be able to read body data
-
 server.use(express.json()); // to support JSON-encoded bodies
 server.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
+
+// 1. register a user
+
+server.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    //1. validation of email and password
+    if (!email) {
+      throw new Error("Email is required");
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      throw new Error("Invalid email format");
+    }
+
+    if (!password) {
+      throw new Error("Password is required");
+    }
+    // 2. check if the user exist
+
+    const user = fakedb.find((user) => user.email === email);
+    if (user) {
+      throw new Error("User already exist");
+    }
+    // 3. if the user exist, hash the password
+    const hashedPassword = await hash(password, 10);
+
+    //4. insert the user in the fakeDb
+    fakedb.push({
+      id: fakedb.length + 1,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).send({ message: "User successfully created" });
+    console.log(fakedb);
+  } catch (error) {
+    res.status(400).send({ error: `${error.message}` });
+  }
+});
 
 server.listen(process.env.PORT, () => {
   console.log(`server listening on port ${process.env.PORT}`);
